31390115e43c4a83dd73ee0eac1c2aa5273c1a11
yahya <yehia.abu.alhassan.01@gmail.com>
2024-01-25 20:47:53 +0200

    users page layout

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,17 @@
-
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { IoIosArrowBack } from "react-icons/io";
 
 export default function Users() {
   return (
-    <div>Users</div>
+    <div className="h-screen w-full bg-blue">
+      <header className=" h-1/4 flex flex-row justify-around items-center">
+        <button className="bg-yellow border-white text-blue"><Link to="../welcome"></Link><IoIosArrowBack/>Go back</button>
+        <button className="bg-yellow border-white text-blue"><Link to="../profile"></Link><CgProfile/>Profile</button>
+        <button className="bg-yellow border-white text-blue"><Link to="../search"></Link><FaSearch/>search</button>
+      </header>
+      
+    </div>
   )
 }
